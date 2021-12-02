de191ee1b39b174a3a37672bc398ab9ab8d6b1cf
Rotashin <mateo.fernandez73@gmail.com>
2021-12-03 00:57:18 +0100

    Ajout controller par défaut et modèle de page

--- app/controllers/router.php
+++ app/controllers/router.php
@@ -1,20 +1,21 @@
 <?php
+require_once File::getApp(array("controllers", "Controller.php"));
 
-$action = $_GET['a'] ?? "";
+$action = $_GET['a'] ?? "home";
 $controller = $_GET['c'] ?? "";
 $controller_class = "Controller" . ucfirst($controller);
 
 if (class_exists($controller_class)) {
-    if (method_exists($controller_class, $action))
-        $controller_class::$action();
-    else {
-        $pagetitle = "Erreur 404";
-        $view = "error";
-        require_once File::getApp(array("views", "view.php"));
-    }
-
-} else {
+  if (method_exists($controller_class, $action))
+    $controller_class::$action();
+  else {
     $pagetitle = "Erreur 404";
     $view = "error";
     require_once File::getApp(array("views", "view.php"));
+  }
+
+} else {
+  $pagetitle = "Erreur 404";
+  $view = "error";
+  require_once File::getApp(array("views", "view.php"));
 }
