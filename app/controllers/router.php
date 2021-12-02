e3b5efec055382e210ee6f8dd6d95eeeaac6ca81
Rotashin <mateo.fernandez73@gmail.com>
2021-12-02 17:02:37 +0100

    mince

--- app/controllers/router.php
+++ app/controllers/router.php
@@ -1,9 +1,4 @@
 <?php
-require_once File::getApp(array("controllers", "ControllerProduct.php"));
-require_once File::getApp(array("controllers", "ControllerUser.php"));
-require_once File::getApp(array("controllers", "ControllerHome.php"));
-require_once File::getApp(array("controllers", "ControllerCart_item.php"));
-require_once File::getApp(array("controllers", "ControllerShopping_cart.php"));
 
 $action = $_GET['a'] ?? "";
 $controller = $_GET['c'] ?? "";
