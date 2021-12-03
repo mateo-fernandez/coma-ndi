ded300dd033901601283bd5fb51f9d046a20085f
Rotashin <22498591+mateo-fernandez@users.noreply.github.com>
2021-12-03 07:48:07 +0100

    Update router.php

--- app/controllers/router.php
+++ app/controllers/router.php
@@ -14,38 +14,13 @@ if (class_exists($controller_class)) {
     if (method_exists($controller_class, $action))
         $controller_class::$action();
     else {
-//        $pagetitle = "Erreur 404";
-//        $view = "error";
-//        require_once File::getApp(array("views", "view.php"));
-        echo "error";
+        $pagetitle = "Erreur 404";
+        $view = "error";
+        require_once File::getApp(array("views", "view.php"));
     }
 
 } else {
-//    $pagetitle = "Erreur 404";
-//    $view = "error";
-//    require_once File::getApp(array("views", "view.php"));
-    echo "error";
+    $pagetitle = "Erreur 404";
+    $view = "error";
+    require_once File::getApp(array("views", "view.php"));
 }
-
-// Code Matéo
-
-//require_once File::getApp(array("controllers", "Controller.php"));
-//
-//$action = $_GET['a'] ?? "home";
-//$controller = $_GET['c'] ?? "";
-//$controller_class = "Controller" . ucfirst($controller);
-//
-//if (class_exists($controller_class)) {
-//  if (method_exists($controller_class, $action))
-//    $controller_class::$action();
-//  else {
-//    $pagetitle = "Erreur 404";
-//    $view = "error";
-//    require_once File::getApp(array("views", "view.php"));
-//  }
-//
-//} else {
-//  $pagetitle = "Erreur 404";
-//  $view = "error";
-//  require_once File::getApp(array("views", "view.php"));
-//}
