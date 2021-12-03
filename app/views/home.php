39d64c540e8fc936af61fba729826395a4ccd290
Rotashin <mateo.fernandez73@gmail.com>
2021-12-03 01:06:13 +0100

    ajout home.php + general.css + inject.php + view de modification du compte

---
+++ app/views/home.php
@@ -0,0 +1,16 @@
+<h1>Accueil</h1>
+<form action="?a=query&c=sengine" method="get">
+    <label>
+        <input id="q" type="text"/>
+        <input type="submit" value="Envoyer" hidden>
+    </label>
+</form>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at consequuntur dolorum est expedita, ipsa ipsum
+    laborum magnam mollitia nemo nulla omnis qui rem repellendus sapiente soluta velit veritatis voluptas.
+    Ad culpa cupiditate dignissimos dolor incidunt molestias mollitia obcaecati officia optio placeat quas, repellat
+    saepe velit. Aspernatur corporis deleniti illo inventore, ipsa laudantium magnam minus molestiae obcaecati
+    reiciendis sequi sint!</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, aspernatur atque dignissimos dolore ea error iusto
+    nesciunt officiis quo recusandae sint temporibus ullam vero. Eius eligendi ipsam modi sint totam?
+    A alias at eaque earum, excepturi facere molestias obcaecati odio provident quibusdam ratione repellat soluta ut!
+    Aliquam amet culpa dolores eos excepturi illum, porro rerum suscipit, tenetur ullam unde vero.</p>
